e5e48034eeccf3957bd5e8f5d2628487b06df3e5
Kyle Smith <axite@axitemedia.com>
2025-06-24 11:53:41 +1000

    remove angy user

--- systems/actual/default.nix
+++ systems/actual/default.nix
@@ -4,25 +4,6 @@
     (self + /modules/actual)
     ./network-config.nix
   ];
-
-  # Add actual user
-  users = {
-    users.actual = {
-      isNormalUser = true;
-      home = "/home/actual";
-      description = "actual User";
-      extraGroups = [
-        "actual"
-        "networkmanager"
-        "wheel"
-      ];
-      openssh.authorizedKeys.keys = [
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINMXEwWst3Kkag14hG+nCtiRX8KHcn6w/rUeZC5Ww7RU axite@axitemedia.com"
-      ];
-    };
-    groups.actual = {
-      name = "actual";
-    };
-  };
+  
   system.stateVersion = "25.11";
 }
