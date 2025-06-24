1d6c0eb17bdd4826ae01355da9cd90bdaf8020be
Kyle Smith <axite@axitemedia.com>
2025-06-24 10:43:47 +1000

    idk bruh

--- modules/actual/default.nix
+++ modules/actual/default.nix
@@ -3,6 +3,13 @@
   services.actual = {
     enable = true;
     openFirewall = true;
-    settings.port = 5006;
+
+    settings = {
+      port = 5006;
+      https = {
+        key = "/home/actual/certs/selfhost.key";
+        cert = "/home/actual/certs/selfhost.key";
+      };
+    };
   };
 }
