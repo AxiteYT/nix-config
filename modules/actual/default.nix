a674b35cbf397a117f96b5002b8a83bef73d1ca5
Kyle Smith <axite@axitemedia.com>
2025-06-24 10:58:45 +1000

    port maybe?

--- modules/actual/default.nix
+++ modules/actual/default.nix
@@ -1,11 +1,26 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 {
+  # Enable access for actual to bind to port 80
+  boot.kernel.sysctl."net.ipv4.ip_unprivileged_port_start" = 0;
+
+  # Allow web port access
+  networking = {
+    firewall = {
+      allowedTCPPorts = [
+        80
+        443
+      ];
+      enable = true;
+    };
+    useHostResolvConf = lib.mkForce false;
+  };
+
   services.actual = {
     enable = true;
     openFirewall = true;
 
     settings = {
-      port = 5006;
+      port = 443;
       https = {
         key = "/home/actual/certs/selfhost.key";
         cert = "/home/actual/certs/selfhost.key";
