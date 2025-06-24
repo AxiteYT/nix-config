fd23b9774a8681f32daba1a976d06c72554bb36f
Kyle Smith <axite@axitemedia.com>
2025-06-24 11:49:55 +1000

    Add actual group

--- systems/actual/default.nix
+++ systems/actual/default.nix
@@ -6,17 +6,23 @@
   ];
 
   # Add actual user
-  users.users.actualuser = {
-    isNormalUser = true;
-    home = "/home/actual";
-    description = "actual User";
-    extraGroups = [
-      "networkmanager"
-      "wheel"
-    ];
-    openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINMXEwWst3Kkag14hG+nCtiRX8KHcn6w/rUeZC5Ww7RU axite@axitemedia.com"
-    ];
+  users = {
+    users.actual = {
+      isNormalUser = true;
+      home = "/home/actual";
+      description = "actual User";
+      extraGroups = [
+        "actual"
+        "networkmanager"
+        "wheel"
+      ];
+      openssh.authorizedKeys.keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINMXEwWst3Kkag14hG+nCtiRX8KHcn6w/rUeZC5Ww7RU axite@axitemedia.com"
+      ];
+    };
+  };
+  groups.actual = {
+    name = "actual";
   };
 
   system.stateVersion = "25.11";
