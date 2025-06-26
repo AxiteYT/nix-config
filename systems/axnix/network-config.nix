d3a1df3cd71c5b09e64829a8e48e3e0a1cbd0daf
Kyle Smith <axite@axitemedia.com>
2025-06-26 14:53:32 +1000

    Bad Networking

--- systems/axnix/network-config.nix
+++ systems/axnix/network-config.nix
@@ -4,5 +4,19 @@
     hostName = "axnix";
     networkmanager.enable = true;
     useDHCP = lib.mkDefault true;
+    firewall = {
+      allowedTCPPortRanges = [
+        {
+          from = 0;
+          to = 64738;
+        }
+      ];
+      allowedUDPPortRanges = [
+        {
+          from = 0;
+          to = 64738;
+        }
+      ];
+    };
   };
 }
