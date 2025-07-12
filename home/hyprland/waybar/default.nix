878ce6432d9f2f59d4915ed8112d22a5f9662e5c
Kyle Smith <axite@axitemedia.com>
2025-07-12 20:49:09 +1000

    Add pavucontrol & pamixer

--- home/hyprland/waybar/default.nix
+++ home/hyprland/waybar/default.nix
@@ -1,6 +1,5 @@
 { ... }:
 {
-
   programs.waybar = {
     enable = true;
     style = builtins.readFile ./style.css;
--- modules/hyprland/default.nix
+++ modules/hyprland/default.nix
@@ -2,6 +2,7 @@
 {
   imports = [
     ./greetd
+    ./waybar
   ];
 
   # Enable hyprland
