eb1c94b30505fa34d9f92a2a5b75612e17c66f54
Kyle Smith <axite@axitemedia.com>
2025-08-13 12:46:13 +1000

    Update  server address

--- systems/common/mounts/nextcloud.nix
+++ systems/common/mounts/nextcloud.nix
@@ -1,6 +1,6 @@
 {
   fileSystems."/media/Nextcloud" = {
-    device = "10.1.10.99:/mnt/Core-Pool/Nextcloud";
+    device = "10.1.10.10:/mnt/Core-Pool/Nextcloud";
     fsType = "nfs";
     options = [ "nfsvers=4.2" ];
   };
--- systems/common/mounts/plex.nix
+++ systems/common/mounts/plex.nix
@@ -1,6 +1,6 @@
 {
   fileSystems."/media/Plex" = {
-    device = "10.1.10.99:/mnt/Core-Pool/Plex";
+    device = "10.1.10.10:/mnt/Core-Pool/Plex";
     fsType = "nfs";
     options = [ "nfsvers=4.2" ];
   };
