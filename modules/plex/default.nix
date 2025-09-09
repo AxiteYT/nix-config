2173638f8b84fd493a7100a640a87f0f62f96359
Kyle Smith <axite@axitemedia.com>
2025-09-09 22:10:39 +1000

    Remove masterpkgs

--- modules/plex/default.nix
+++ modules/plex/default.nix
@@ -1,12 +1,4 @@
 { pkgs, inputs, ... }:
-let
-  pkgsMaster = import inputs.pkgsMaster {
-    system = pkgs.stdenv.hostPlatform.system;
-    config = {
-      allowUnfree = true;
-    };
-  };
-in
 {
   services.plex = {
     enable = true;
