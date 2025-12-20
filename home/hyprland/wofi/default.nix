e93d1361bcb8c1ada150e8ea67c773153473e474
Kyle Smith <axite@axitemedia.com>
2025-12-21 08:33:44 +1100

    re-theme wofi

--- home/hyprland/wofi/default.nix
+++ home/hyprland/wofi/default.nix
@@ -27,72 +27,60 @@
     };
     ".config/wofi/style.css" = {
       text = ''
-        @define-color base00 #282828;
-        @define-color base01 #3C3836;
-        @define-color base02 #504945;
-        @define-color base03 #665C54;
-        @define-color base04 #BDAE93;
-        @define-color base06 #D5C4A1;
-        @define-color base06 #EBDBB2;
-        @define-color base07 #FBF1C7;
-        @define-color base08 #FB4934;
-        @define-color base09 #FE8019;
-        @define-color base0A #FABD2F;
-        @define-color base0B #B8BB26;
-        @define-color base0C #8EC07C;
-        @define-color base0D #83A598;
-        @define-color base0E #D3869B;
-        @define-color base0F #D65D0E;
-
-        window {
-            opacity: 0.9;
-            border:  0px;
-            border-radius: 10px;
-            font-family: monospace;
-            font-size: 18px;
+        /* The name of the window itself */
+        #window {
+          background-color: rgba(24, 24, 24, 0.6);
+          box-shadow: 0 10px 15px -3px rgba(0, 0, 0, 0.1),
+            0 4px 6px -4px rgba(0, 0, 0, 0.1);
+          border-radius: 1rem;
+          font-size: 1.2rem;
+          /* The name of the box that contains everything */
+        }
+        #window #outer-box {
+          /* The name of the search bar */
+          /* The name of the scrolled window containing all of the entries */
+        }
+        #window #outer-box #input {
+          background-color: rgba(24, 24, 24, 0.6);
+          color: #f2f2f2;
+          border: none;
+          border-bottom: 1px solid rgba(24, 24, 24, 0.2);
+          padding: 0.8rem 1rem;
+          font-size: 1.5rem;
+          border-radius: 1rem 1rem 0 0;
+        }
+        #window #outer-box #input:focus,
+        #window #outer-box #input:focus-visible,
+        #window #outer-box #input:active {
+          border: none;
+          outline: 2px solid transparent;
+          outline-offset: 2px;
+        }
+        #window #outer-box #scroll {
+          /* The name of the box containing all of the entries */
+        }
+        #window #outer-box #scroll #inner-box {
+          /* The name of all entries */
+          /* The name of all boxes shown when expanding  */
+          /* entries with multiple actions */
+        }
+        #window #outer-box #scroll #inner-box #entry {
+          color: #fff;
+          background-color: rgba(24, 24, 24, 0.1);
+          padding: 0.6rem 1rem;
+          /* The name of all images in entries displayed in image mode */
+          /* The name of all the text in entries */
+        }
+        #window #outer-box #scroll #inner-box #entry #img {
+          width: 1rem;
+          margin-right: 0.5rem;
+        }
+        #window #outer-box #scroll #inner-box #entry:selected {
+          color: #fff;
+          background-color: rgba(255, 255, 255, 0.1);
+          outline: none;
         }
 
-        #input {
-        	border-radius: 10px 10px 0px 0px;
-            border:  0px;
-            padding: 10px;
-            margin: 0px;
-            font-size: 28px;
-        	color: #8EC07C;
-        	background-color:rgb(45, 76, 94);
-        }
-
-        #inner-box {
-        	margin: 0px;
-        	color: @base06;
-        	background-color: @base00;
-        }
-
-        #outer-box {
-        	margin: 0px;
-        	background-color: @base00;
-            border-radius: 10px;
-        }
-
-        #selected {
-        	background-color: #608787;
-        }
-
-        #entry {
-        	padding: 0px;
-            margin: 0px;
-        	background-color: @base00;
-        }
-
-        #scroll {
-        	margin: 5px;
-        	background-color: @base00;
-        }
-
-        #text {
-        	margin: 0px;
-        	padding: 2px 2px 2px 10px;
-        }
       '';
     };
     ".config/wofi/power.sh" = {
