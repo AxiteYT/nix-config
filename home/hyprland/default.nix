bab1ae539873acf173017af779664ed2f9d82095
Kyle Smith <axite@axitemedia.com>
2025-12-21 09:34:48 +1100

    nix fmt

--- home/hyprland/default.nix
+++ home/hyprland/default.nix
@@ -16,21 +16,19 @@ in
       variables = [ "--all" ];
     };
     xwayland.enable = true;
-    extraConfig =
-      (builtins.readFile ./hyprland.conf)
-      + ''
-        # Stylix palette overrides
-        general {
-            col.active_border = ${argb "ff" colors.base0D}
-            col.inactive_border = ${argb "cc" colors.base03}
-        }
+    extraConfig = (builtins.readFile ./hyprland.conf) + ''
+      # Stylix palette overrides
+      general {
+          col.active_border = ${argb "ff" colors.base0D}
+          col.inactive_border = ${argb "cc" colors.base03}
+      }
 
-        decoration {
-            shadow {
-                color = ${argb "ee" colors.base00}
-            }
-        }
-      '';
+      decoration {
+          shadow {
+              color = ${argb "ee" colors.base00}
+          }
+      }
+    '';
   };
 
   # Wallpaper is handled by awww; keep hyprpaper disabled to avoid conflicts.
