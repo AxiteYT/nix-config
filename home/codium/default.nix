ab3874db5d4738093aa53a4ed409223edbb1ab65
Kyle Smith <axite@axitemedia.com>
2025-12-26 15:46:51 +1100

    Set confirmDelete to fasle

--- home/codium/default.nix
+++ home/codium/default.nix
@@ -1,4 +1,9 @@
-{ config, lib, pkgs, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 {
   stylix.targets.vscode = {
     enable = true;
@@ -8,33 +13,41 @@
   programs.vscode = {
     enable = true;
     package = pkgs.vscodium;
-    profiles.default.extensions = with pkgs.vscode-extensions; [
-      aaron-bond.better-comments
-      donjayamanne.githistory
-      esbenp.prettier-vscode
-      jbockle.jbockle-format-files
-      jnoortheen.nix-ide
-      kamikillerto.vscode-colorize
-      yzhang.markdown-all-in-one
-    ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
-      {
-        name = "regex";
-        publisher = "chrmarti";
-        version = "0.6.0";
-        sha256 = "Exw2CuSsJKphgSTAK72DxkO+oHv2I4LGo6BV3NvYVCs=";
-      }
-      {
-        name = "hyprlang-vscode";
-        publisher = "fireblast";
-        version = "0.0.3";
-        sha256 = "iMCyomgMGGUXaVqq1l7bgyvFgZa/W/eWHaqkA5RmExE=";
-      }
-      {
-        name = "chatgpt";
-        publisher = "openai";
-        version = "0.5.56";
-        sha256 = "FAy2Cf2XnOnctBBATloXz8y4cLNHBoXAVnlw42CQzN8=";
-      }
-    ];
+    profiles.default = {
+      extensions =
+        with pkgs.vscode-extensions;
+        [
+          aaron-bond.better-comments
+          donjayamanne.githistory
+          esbenp.prettier-vscode
+          jbockle.jbockle-format-files
+          jnoortheen.nix-ide
+          kamikillerto.vscode-colorize
+          yzhang.markdown-all-in-one
+        ]
+        ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+          {
+            name = "regex";
+            publisher = "chrmarti";
+            version = "0.6.0";
+            sha256 = "Exw2CuSsJKphgSTAK72DxkO+oHv2I4LGo6BV3NvYVCs=";
+          }
+          {
+            name = "hyprlang-vscode";
+            publisher = "fireblast";
+            version = "0.0.3";
+            sha256 = "iMCyomgMGGUXaVqq1l7bgyvFgZa/W/eWHaqkA5RmExE=";
+          }
+          {
+            name = "chatgpt";
+            publisher = "openai";
+            version = "0.5.56";
+            sha256 = "FAy2Cf2XnOnctBBATloXz8y4cLNHBoXAVnlw42CQzN8=";
+          }
+        ];
+      userSettings = {
+        "explorer.confirmDelete" = false;
+      };
+    };
   };
 }
