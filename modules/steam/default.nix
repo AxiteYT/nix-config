cfc4af8095e7ba9a49ea3042689f28f1968bbe30
Kyle Smith <axite@axitemedia.com>
2025-12-26 14:02:22 +1100

    Add more steam packages

--- modules/steam/default.nix
+++ modules/steam/default.nix
@@ -3,11 +3,20 @@
   programs = {
     steam = {
       enable = true;
+      protontricks.enable = true;
       gamescopeSession.enable = true;
       remotePlay.openFirewall = true;
       dedicatedServer.openFirewall = true;
       localNetworkGameTransfers.openFirewall = true;
+      extraCompatPackages = with pkgs; [
+        proton-ge-bin
+      ];
+      extraPackages = with pkgs; [
+        gamescope
+        mangohud
+      ];
     };
+    gamescope.enable = true;
     gamemode.enable = true;
   };
 }
