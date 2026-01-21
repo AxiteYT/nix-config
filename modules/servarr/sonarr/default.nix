e7d4efde3528ac4c5d98837e03de878755067165
AxiteYT <Axite@axitemedia.com>
2026-01-22 09:14:32 +1100

    pin sqlite version

--- modules/servarr/sonarr/default.nix
+++ modules/servarr/sonarr/default.nix
@@ -1,7 +1,19 @@
 {
-  services.sonarr = {
-    enable = true;
-    group = "servarr";
-    openFirewall = true;
-  };
+  services.sonarr =
+    #TODO: Remove sqlite pinning once it resolved upstream: https://github.com/NixOS/nixpkgs/issues/481098
+    let
+      sqlite-3-50 = pkgs.sqlite.overrideAttrs (old: {
+        version = "3.50.0";
+        src = pkgs.fetchurl {
+          url = "https://sqlite.org/2025/sqlite-autoconf-3500000.tar.gz";
+          sha256 = "09w32b04wbh1d5zmriwla7a02r93nd6vf3xqycap92a3yajpdirv";
+        };
+      });
+    in
+    {
+      enable = true;
+      package = pkgs.unstable.sonarr.override { sqlite = sqlite-3-50; };
+      group = "servarr";
+      openFirewall = true;
+    };
 }
