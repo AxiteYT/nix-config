61bcd9dafd1d72d019739b49e31548525b5a38a4
Kyle Smith <axite@axitemedia.com>
2026-05-04 13:49:58 +1000

    Switch to vscodium option

--- home/codium/default.nix
+++ home/codium/default.nix
@@ -10,9 +10,8 @@
     profileNames = [ "default" ];
   };
 
-  programs.vscode = {
+  programs.vscodium = {
     enable = true;
-    package = pkgs.vscodium;
     profiles.default = {
       extensions =
         with pkgs.vscode-extensions;
